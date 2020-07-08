12019e301880ac133896693be2299f5e61cc000b
CaptureOSM <64198462+CaptureOSM@users.noreply.github.com>
2020-07-08 16:40:20 +0200

    Add files via upload

---
+++ st_sample.R
@@ -0,0 +1,25 @@
+st_read("s.shp")
+s %>% 
+  group_by(BZT3 )%>% 
+  nest() %>%
+  mutate(samp = map(data, st_sample, 5))
+
+
+s2 <- 
+  s %>% 
+  group_by(BZT3)%>% 
+  nest() 
+
+
+s2[1:10,] %>%
+  mutate(samp = map(data, st_sample, 5))
+
+
+s2[[2]][[11]] %>%
+  slice(1:nrow(s2[[2]][[11]])) %>%
+  st_sample(5)
+  
+s2[[2]][[12]] %>%
+  slice(1:nrow(s2[[2]][[12]])) %>%
+  st_sample(5)
+
